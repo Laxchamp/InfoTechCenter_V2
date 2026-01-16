b465ee94b87f28e4a97c84ba2f93474c31c99309
Rowe <coledrowe@gmail.com>
2026-01-16 13:34:25 -0500

    Added Color Thanks AI

--- withorwithnotmaranaria.py
+++ withorwithnotmaranaria.py
@@ -1,26 +1,34 @@
 # Welcome Branch
-
-#Libraries Imported Here
+# Libraries Imported Here
 import sys
 import time
 
-print("\nWelcome Branch - Developer Cole")
-print("\nWelcome to InfoTechCenter V.1.0")
+# ANSI color codes
+RESET = "\033[0m"
+GREEN = "\033[92m"
+CYAN = "\033[96m"
+YELLOW = "\033[93m"
+MAGENTA = "\033[95m"
 
+print(f"\n{MAGENTA}Welcome Branch - Developer Cole{RESET}")
+print(f"\n{CYAN}Welcome to InfoTechCenter V.1.0{RESET}")
 
 x = 0
 ellipsis = 0
 
 while x != 20:
-    x += 1 
-    ellipsisMessage = ("InfoTehCenter OS Booting" + "." * ellipsis)
-    ellipsis += 1 
+    x += 1
+    ellipsisMessage = f"{YELLOW}InfoTechCenter OS Booting{'.' * ellipsis}{RESET}"
+    ellipsis += 1
+
     sys.stdout.write("\r\033[K" + ellipsisMessage)
-    sys.stdout.flush()   
+    sys.stdout.flush()
     time.sleep(.5)
+
     if ellipsis == 4:
         ellipsis = 0
+
     if x == 20:
-        print("\nOperating System Booted Up - Retina Access Granted")
+        print(f"\n{GREEN}Operating System Booted Up - Retina Access Granted{RESET}")
 
 
